27fe366a26db2548dc6ce4bab4e86048d415ca0a
jHallsG <134239144+jHallsG@users.noreply.github.com>
2025-03-19 13:38:13 +0800

    feat(service): created an AI chatter using Ollama and Llama3.1:8b

---
+++ src/main/java/com/springai/SpringAIDemo/SpringAiProjectOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.springai.SpringAIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiProjectOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiProjectOllamaApplication.class, args);
+	}
+
+}
